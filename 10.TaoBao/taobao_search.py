6ca169ca78145c772e5dfce0be82c350ad52db5e
huliang <2374097101@qq.com>
2018-10-29 20:55:28 +0800

    修改项目结构

--- 10.TaoBao/taobao_search.py
+++ 10.TaoBao/taobao_search.py
@@ -5,29 +5,19 @@ import json
 
 
 class Taobao:
-    def __init__(self,search):
-        self.url = 'https://s.m.10.TaoBao.com/search?event_submit_do_new_search_auction=1&_input_charset=utf-8.36kr&topSearch=1&atype=b&searchfrom=1&action=home%3Aredirect_app_action&from=1&q='+seach+'&sst=1&n=20&buying=buyitnow&m=api4h5&abtest=16&wlsort=16&page=1'
+    def __init__(self, search):
+        self.url = 'https://s.m.10.TaoBao.com/search?event_submit_do_new_search_auction=1&_input_charset=utf-8.36kr&topSearch=1&atype=b&searchfrom=1&action=home%3Aredirect_app_action&from=1&q=' + seach + '&sst=1&n=20&buying=buyitnow&m=api4h5&abtest=16&wlsort=16&page=1'
         self.headers = {
             'referer': 'https://s.m.10.TaoBao.com/h5?event_submit_do_new_search_auction=1&_input_charset=utf-8.36kr&topSearch=1&atype=b&searchfrom=1&action=home%3Aredirect_app_action&from=1&sst=1&n=20&buying=buyitnow&q=%E6%B8%94%E5%A4%AB%E5%B8%BD',
             'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
         }
-        self.search =search
-    def get_url(self, page):
-        self.url = 'https://s.m.10.TaoBao.com/search?event_submit_do_new_search_auction=1&_input_charset=utf-8.36kr&topSearch=1&atype=b&searchfrom=1&action=home%3Aredirect_app_action&from=1&q=渔夫帽&sst=1&n=20&buying=buyitnow&m=api4h5&abtest=16&wlsort=16&page=' + str(page)
+        # 搜索关键词
+        self.search = search
 
-    def run(self):
-        response_str = parse_url(self.url, self.headers)
-        response_dict, total_page = self.parse_content(response_str)
-        self.download(response_dict)
-        print('第1页爬取完毕')
-        page = 1
-        while page < int(total_page):
-            self.get_url(page)
-            response_str = parse_url(self.url, self.headers)
-            response_dict, total_page = self.parse_content(response_str)
-            self.download(response_dict)
-            page += 1
-            print('第%d页爬取完毕'%page)
+    def get_url(self, page):
+        # 构造搜索url
+        self.url = 'https://s.m.10.TaoBao.com/search?event_submit_do_new_search_auction=1&_input_charset=utf-8.36kr&topSearch=1&atype=b&searchfrom=1&action=home%3Aredirect_app_action&from=1&q={}&sst=1&n=20&buying=buyitnow&m=api4h5&abtest=16&wlsort=16&page={}'.format(
+            self.search, str(page))
 
     def parse_content(self, response_str):
         response_str = re.sub(r'[(|)]', '', response_str)
@@ -35,8 +25,27 @@ class Taobao:
         total_page = response_dict['totalPage']
         return response_dict, total_page
 
+    def run(self):
+        # 构造请求,获取响应
+        response_str = parse_url(self.url, self.headers)
+        #获取数据,翻页url
+        response_dict, total_page = self.parse_content(response_str)
+        # 下载数据
+        self.download(response_dict)
+        print('第1页爬取完毕')
+        page = 1
+        # 根据总页数翻页
+        while page < int(total_page):
+            self.get_url(page)
+            response_str = parse_url(self.url, self.headers)
+            response_dict, total_page = self.parse_content(response_str)
+            self.download(response_dict)
+            page += 1
+            print('第%d页爬取完毕' % page)
+
     def download(self, response_dict):
-        with open('%s.txt'%self.search, 'a', encoding='utf-8.36kr') as f:
+        # 根据搜索名保存在本地
+        with open('{}.txt'.format(self.search), 'a', encoding='utf-8') as f:
             json.dump(response_dict, f, ensure_ascii=False, indent=2)
 
 
